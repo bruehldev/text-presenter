0659e2a80e49418fc35b4c6e7eb5fc1bea41a394
Daniel Brühl <bruehl.dev@gmail.com>
2023-12-15 21:12:43 +0100

    feat(main_window): toggle init

--- src/windows/main_window.py
+++ src/windows/main_window.py
@@ -1,21 +1,31 @@
+import tkinter as tk
 from tts_manager import generate_tts
 from src.windows.tk.base_window import BaseWindow
 from src.windows.tk.text_input import TextInputWindow
-import tkinter as tk
 
 
 class MainWindow(BaseWindow):
     def __init__(self, master):
         super().__init__(master, "Options", "config/root.conf")
-        self.button1 = tk.Button(
-            self.frame, text="New Window", width=25, command=self.toggle_text_input
-        )
-        self.button1.pack()
+
         self.frame.pack()
         self.text_input_window = TextInputWindow(tk.Toplevel(self.master))
+        self.text_input_window_state = self.text_input_window.master.state()
+        self.button1 = tk.Button(
+            self.frame,
+            text="",
+            command=lambda: self.toggle_button("Text Input"),
+        )
+        if self.text_input_window_state == "normal":
+            self.button1.config(text=f"Close Text Input")
+        else:
+            self.button1.config(text="Open Text Input")
+        self.button1.pack()
 
-    def toggle_text_input(self):
+    def toggle_button(self, name):
         if self.text_input_window.master.state() == "normal":
             self.text_input_window.master.withdraw()
+            self.button1.config(text=f"Open {name}")
         else:
             self.text_input_window.master.deiconify()
+            self.button1.config(text=f"Close {name}")
