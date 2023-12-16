b12acb807724b0acc76d20f3ecd8ba8b172cb862
Daniel Brühl <bruehl.dev@gmail.com>
2023-12-16 21:19:30 +0100

    feat(text_input): make text input non editable

--- src/windows/tk/text_input.py
+++ src/windows/tk/text_input.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from src.windows.tk.base_window import BaseWindow
 import nltk
 from src.services.headline_generator import generate_headline
-
+from audio_manager import delete_audio_files
 
 nltk.download("punkt")
 
@@ -10,7 +10,7 @@ nltk.download("punkt")
 class TextInputWindow(BaseWindow):
     def __init__(self, master, text_window, audio_window):
         super().__init__(master, "Text Input", "config/text_input.conf")
-        self.text_input = tk.Text(self.master)
+        self.text_input = tk.Text(self.master, state=tk.NORMAL)
         self.text_input.pack()
         self.text_window = text_window
         self.audio_window = audio_window
@@ -23,10 +23,14 @@ class TextInputWindow(BaseWindow):
         self.send_button.pack()
 
     def update_text_window(self):
+        # TODO: delete audio files or use Apply to Process Audio. I keep it for faster testing
+        # delete_audio_files()
         self.text = self.text_input.get("1.0", "end-1c")
         self.sentences = nltk.sent_tokenize(self.text)
+        self.text_window.text_widget.config(state=tk.NORMAL)
         self.text_window.text_widget.delete("1.0", "end")
         self.text_window.text_widget.insert("1.0", self.text_input.get("1.0", "end-1c"))
+        self.text_window.text_widget.config(state=tk.DISABLED)
         self.audio_window.sentences = self.sentences
 
         # generate headline
