3b6331abd14c1a98600f8f02c81aef408c97a5c2
Daniel Brühl <bruehl.dev@gmail.com>
2024-01-18 19:15:05 +0100

    feat(rsvp): implement themes

--- src/windows/tk/rsvp_window.py
+++ src/windows/tk/rsvp_window.py
@@ -1,4 +1,5 @@
-import tkinter as tk
+from tkinter import ttk, SOLID, LEFT, BOTH, TOP, CENTER, BOTTOM
+
 from src.windows.tk.base_window import BaseWindow
 from src.services.config_manager import get_config_parameter, set_config_parameter
 
@@ -6,60 +7,62 @@ from src.services.config_manager import get_config_parameter, set_config_paramet
 class rsvpWindow(BaseWindow):
     def __init__(self, master):
         super().__init__(master, "rsvp Window", "config/rsvp_window.conf")
+        # Frame to hold the label with a border
+        self.frame = ttk.Frame(self.master, padding=10, border=1)
+        self.frame.pack(fill=BOTH, expand=True)
+
         self.word = "A sentence example for testing wraplength; it's quite lengthy, demonstrating how the feature adjusts text in the Tkinter window."
 
         # Initial wraplength
         self.wraplength = 300
         self.font_size = 24  # Initial font size
 
-        # Frame to hold the label with a border
-        self.frame = tk.Frame(self.master, bd=2, relief=tk.SOLID)
-        self.frame.pack(fill=tk.BOTH, expand=True)
+        bg_color = self.master.winfo_toplevel().cget("bg")
 
-        self.word_label = tk.Label(
+        self.word_label = ttk.Label(
             self.frame,
             text=self.word,
             font=("Helvetica", self.font_size),
             wraplength=self.wraplength,
-            justify=tk.LEFT,
+            justify=CENTER,
         )
-        self.word_label.pack(fill=tk.BOTH, expand=True)
+        self.word_label.place(relx=0.5, rely=0.5, anchor=CENTER)
 
         # Label to display wraplength
-        self.wraplength_label = tk.Label(
-            self.master, text=f"Wraplength: {self.wraplength}"
+        self.wraplength_label = ttk.Label(
+            self.frame, text=f"Wraplength: {self.wraplength}"
         )
-        self.wraplength_label.pack(side=tk.TOP)
+        self.wraplength_label.pack(side=BOTTOM)
 
         # Label to display font size
-        self.font_size_label = tk.Label(
-            self.master, text=f"Text Size: {self.font_size}"
+        self.font_size_label = ttk.Label(
+            self.frame, text=f"Text Size: {self.font_size}"
         )
-        self.font_size_label.pack(side=tk.TOP)
+        self.font_size_label.pack(side=BOTTOM)
 
         # Button to increase wraplength
-        self.increase_wraplength_button = tk.Button(
-            self.master, text="Increase Wraplength", command=self.increase_wraplength
+        self.increase_wraplength_button = ttk.Button(
+            self.frame, text="Increase Wraplength", command=self.increase_wraplength
         )
-        self.increase_wraplength_button.pack(side=tk.LEFT)
+        self.increase_wraplength_button.pack(side=BOTTOM)
 
         # Button to decrease wraplength
-        self.decrease_wraplength_button = tk.Button(
-            self.master, text="Decrease Wraplength", command=self.decrease_wraplength
+        self.decrease_wraplength_button = ttk.Button(
+            self.frame, text="Decrease Wraplength", command=self.decrease_wraplength
         )
-        self.decrease_wraplength_button.pack(side=tk.LEFT)
+        self.decrease_wraplength_button.pack(side=BOTTOM)
 
         # Button to increase text size
-        self.increase_text_size_button = tk.Button(
-            self.master, text="Increase Text Size", command=self.increase_text_size
+        self.increase_text_size_button = ttk.Button(
+            self.frame, text="Increase Text Size", command=self.increase_text_size
         )
-        self.increase_text_size_button.pack(side=tk.LEFT)
+        self.increase_text_size_button.pack(side=BOTTOM)
 
         # Button to decrease text size
-        self.decrease_text_size_button = tk.Button(
-            self.master, text="Decrease Text Size", command=self.decrease_text_size
+        self.decrease_text_size_button = ttk.Button(
+            self.frame, text="Decrease Text Size", command=self.decrease_text_size
         )
-        self.decrease_text_size_button.pack(side=tk.LEFT)
+        self.decrease_text_size_button.pack(side=BOTTOM)
 
         self.load_wraplength()
         self.load_font_size()
