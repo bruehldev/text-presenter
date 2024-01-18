7c21d68da485a3e6a72b9084d738f758be37a1c8
Daniel Brühl <bruehl.dev@gmail.com>
2024-01-18 20:33:44 +0100

    feat(text_window): set frame

--- src/windows/tk/text_window.py
+++ src/windows/tk/text_window.py
@@ -1,16 +1,17 @@
-import tkinter as tkk
+from tkinter import ttk, Text
 from src.windows.tk.base_window import BaseWindow
 
 
 class TextWindow(BaseWindow):
     def __init__(self, master):
         super().__init__(master, "Text Window", "config/text_window.conf")
-        self.frame = tkk.Frame(self.master)
+        self.frame = ttk.Frame(self.master)
+        self.frame.pack(fill="both", expand=True)
 
         # headline
-        self.headline = tkk.Label(self.master, text="Generated Headline")
+        self.headline = ttk.Label(self.frame, text="Generated Headline")
         self.headline.pack()
 
-        self.text_widget = tkk.Text(self.master, wrap="word")
+        self.text_widget = Text(self.frame, wrap="word")
         self.text_widget.configure(bg="black", fg="white")
         self.text_widget.pack(fill="both", expand=True)
