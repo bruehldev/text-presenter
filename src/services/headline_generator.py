07ae901df8141701dd50d96a480104314c2c2e8c
Daniel Brühl <bruehl.dev@gmail.com>
2024-01-24 18:04:38 +0100

    feat(headline): improve a bit

--- src/services/headline_generator.py
+++ src/services/headline_generator.py
@@ -1,23 +1,21 @@
-import re
-from transformers import AutoTokenizer, T5ForConditionalGeneration
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import nltk
 
 
 def generate_headline(text):
-    WHITESPACE_HANDLER = lambda k: re.sub("\s+", " ", re.sub("\n+", " ", k.strip()))
-    model_name = "JulesBelveze/t5-small-headline-generator"
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = T5ForConditionalGeneration.from_pretrained(model_name)
-    input_ids = tokenizer(
-        [WHITESPACE_HANDLER(text)],
-        return_tensors="pt",
-        padding="max_length",
-        truncation=True,
-        max_length=384,
-    )["input_ids"]
-    output_ids = model.generate(
-        input_ids=input_ids, max_length=84, no_repeat_ngram_size=2, num_beams=4
-    )[0]
-    summary = tokenizer.decode(
-        output_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False
+    tokenizer = AutoTokenizer.from_pretrained(
+        "fabiochiu/t5-small-medium-title-generation"
     )
-    return summary
+    model = AutoModelForSeq2SeqLM.from_pretrained(
+        "fabiochiu/t5-small-medium-title-generation"
+    )
+    inputs = ["summarize: " + text]
+
+    inputs = tokenizer(inputs, truncation=True, return_tensors="pt")
+    output = model.generate(
+        **inputs, num_beams=10, do_sample=True, min_length=5, max_length=32
+    )
+    decoded_output = tokenizer.batch_decode(output, skip_special_tokens=True)[0]
+    predicted_title = nltk.sent_tokenize(decoded_output.strip())[0]
+
+    return predicted_title
